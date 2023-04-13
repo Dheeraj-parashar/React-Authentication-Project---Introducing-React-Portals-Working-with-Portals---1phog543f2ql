9d51a942abcbfb3c9544371b86e5f121d4d720d4
Dheeraj-parashar <127424719+Dheeraj-parashar@users.noreply.github.com>
2023-04-14 00:19:13 +0530

    update

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,16 @@
+import React from "react";
+function PrivateRoute(){
+    
+    if(window.localStorage.getItem(username)){
+    return(
+        <Profile/>
+    )
+
+    }
+    else {
+        return(
+            <Login/>
+        )
+    }
+}
 
